33298144299a597ac21f48bf44b68badc803fc61
laci <11998934+laszlo-san@users.noreply.github.com>
2019-03-24 23:08:15 +0100

    rebased and sing commit

--- templating/routes/admin.js
+++ templating/routes/admin.js
@@ -1,20 +1,18 @@
-const path = require('path');
 
 const express = require('express');
-
-const rootDir = require('../util/path');
+ require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post('/add-product', (req, res) => {
   products.push({ title: req.body.title });
   res.redirect('/');
 });
