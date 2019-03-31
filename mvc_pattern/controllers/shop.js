f46b32801da7d28ee83c77803bec8ed7862e89be
laci <11998934+laszlo-san@users.noreply.github.com>
2019-03-31 17:27:32 +0200

    Added the prod detail veiw and the route and controller to access date for it to render

--- mvc_pattern/controllers/shop.js
+++ mvc_pattern/controllers/shop.js
@@ -10,6 +10,14 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products'});
+  });
+  
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -40,3 +48,4 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
