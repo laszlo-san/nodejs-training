caf27557e24e5aef43df52a74ac05f98e298d462
laci <11998934+laszlo-san@users.noreply.github.com>
2019-04-02 18:05:13 +0200

    added edit product controller and uptadet the Product save method

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
+exports.getAddProduct = (req, res) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
@@ -8,17 +8,17 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = (req, res) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
-exports.getEditProduct = (req, res, next) => {
+exports.getEditProduct = (req, res) => {
   const editMode = req.query.edit;
 
   if (!editMode) {
@@ -41,7 +41,25 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res, next) => {
+exports.postEditProduct = (req, res) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
+
+exports.getProducts = (req, res) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
       prods: products,
