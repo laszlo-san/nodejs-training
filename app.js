f3defe2290ebc74ef22b3f351b9712c29bc444a0
laci <11998934+laszlo-san@users.noreply.github.com>
2019-04-10 22:58:18 +0200

    added add to cart functionality back

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
 
 sequelize
-  .sync({force:true})
+  // .sync({force:true})
+  .sync()
   .then(result => {
     return User.findByPk(1);
   })
@@ -54,6 +55,9 @@ sequelize
     return user;
   })
   .then(user => {
+    return user.createCart();
+  })
+  .then(cart=> {
     app.listen(3000);
   })
   .catch(err => {
