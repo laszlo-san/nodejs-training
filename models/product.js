6eda5de20818098ad60e8708754b3a090a9fab96
laszlo-san <11998934+laszlo-san@users.noreply.github.com>
2019-04-26 13:36:08 +0200

    findbyID not working properly right now, need further investigation

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -24,11 +25,26 @@ class Product {
   static fetchAll() {
     const db = getDb();
     return db
-      .collection('products')
+      .collection("products")
       .find()
       .toArray()
       .then(products => {
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectID(prodId) })
+      .next()
+      .then(product => {
+        return product;
       })
       .catch(err => {
         console.log(err);
