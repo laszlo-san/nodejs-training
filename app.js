14f999686fd4af59784eefddccedaf74c3935344
laszlo-san <11998934+laszlo-san@users.noreply.github.com>
2019-05-02 11:33:53 +0200

    added users model back in, products have a user reference

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 
 const app = express();
@@ -19,13 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('5ccab7668044a02263128afa')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,41 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+
+    return db.collection('users')
+      .insertOne(this)
+      .than(res => {
+        console.log('user inserted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+
+    return db.collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then(result=> {
+        console.log(result);
+        return result;
+        
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
