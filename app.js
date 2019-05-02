e71c8b854067ebbabc4d61c1105fa31ee4ace870
laszlo-san <11998934+laszlo-san@users.noreply.github.com>
2019-05-02 15:11:09 +0200

    added back add to cart functionality

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5ccab7668044a02263128afa')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -4,15 +4,18 @@ const ObjectId = mongodb.ObjectId;
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
 
-    return db.collection('users')
+    return db
+      .collection('users')
       .insertOne(this)
       .than(res => {
         console.log('user inserted');
@@ -22,15 +25,44 @@ class User {
       });
   }
 
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+
+    const updatedCart = { items: updatedCartItems };
+    const db = getDb();
+
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
   static findById(userId) {
     const db = getDb();
 
-    return db.collection('users')
+    return db
+      .collection('users')
       .findOne({ _id: new ObjectId(userId) })
-      .then(result=> {
+      .then(result => {
         console.log(result);
         return result;
-        
       })
       .catch(err => {
         console.log(err);
