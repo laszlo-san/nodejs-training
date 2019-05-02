3cb4d0b70b4b607753f6b49996ffd13cd8ab202f
laszlo-san <11998934+laszlo-san@users.noreply.github.com>
2019-05-02 09:21:53 +0200

    fixed product model to return a Promise

--- models/product.js
+++ models/product.js
@@ -61,7 +61,7 @@ class Product {
 
   static deleteById(prodId) {
     const db = getDb();
-    db.collection('products')
+    return db.collection('products')
       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
       .then(result => {
         console.log('Deleted');
