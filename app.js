6a863ea261bb418aac8c156646139662195462bb
laci <11998934+laszlo-san@users.noreply.github.com>
2019-05-14 16:44:54 +0200

    ended sessions and cookies

--- app.js
+++ app.js
@@ -36,6 +36,17 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -49,8 +60,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Laci',
+          email: 'lac@test.com',
           cart: {
             items: []
           }
