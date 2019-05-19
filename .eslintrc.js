61bc67382cba48f2b722d68314abc8e33953d4d7
laci <11998934+laszlo-san@users.noreply.github.com>
2019-05-20 00:11:37 +0200

    added soem basic route, json bodyparser and CORS allowance

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-unused-vars': ['error', { args: 'none' }],
+    'object-shorthand': ['error', 'never']
+  }
+};
