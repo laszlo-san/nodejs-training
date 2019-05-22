ec819326145a171b710ec7f1e83d24a9465e2a33
laci <11998934+laszlo-san@users.noreply.github.com>
2019-05-22 19:37:17 +0200

    starting graphql by cleaning code

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -49,9 +46,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -66,12 +60,7 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(result => {
-    const server = app.listen(8080);
-    // eslint-disable-next-line global-require
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client connected');
-    });
+    app.listen(8080);
   })
   .catch(err => {
     console.log(err);
