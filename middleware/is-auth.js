b72c28cbecd94a7de5cb29ba21c89aacebe87c66
laci <11998934+laszlo-san@users.noreply.github.com>
2019-06-10 15:59:13 +0200

    starting deployment from a previouse snapshot

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,26 +1,6 @@
-const jwt = require('jsonwebtoken');
-
 module.exports = (req, res, next) => {
-  const authHeader = req.get('Authorization');
-  if (!authHeader) {
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
-  }
-  const token = req.get('Authorization').split(' ')[1];
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, 'sadfl4lkjlkdjfoihoioobgobbkjkjiIEJIOfds44tg4io+Rhngoi4tfgfgv');
-  } catch (err) {
-    err.statusCode = 500;
-    throw err;
-  }
-
-  if (!decodedToken) {
-    const error = new Error('Not authenticated.');
-    error.statusCode = 401;
-    throw error;
-  }
-  req.userId = decodedToken.userId;
-  next();
-};
+    if (!req.session.isLoggedIn) {
+        return res.redirect('/login');
+    }
+    next();
+}
